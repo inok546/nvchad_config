da4f61218887e9e7e4ee7df44a7ff285cc52454d
Inok546 <inok824@gmail.com>
2024-08-29 15:06:36 +0300

    added F-key duplicates for existing debug mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,7 +5,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- debugger
+-- Debugger
+map("n", "<leader>dq", "<cmd> DapTerminate <CR>", { desc = "Terminate the debugger" })
 map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Add/remove breakpoint at line" })
 -- TODO: Сделать чтобы автоматом компилился текущий файл при вызове дебагера
 map("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "Start/continue the debugger" })
@@ -13,7 +14,16 @@ map("n", "<leader>dn", "<cmd> DapStepOver <CR>", { desc = "Step over" })
 map("n", "<leader>di", "<cmd> DapStepInto <CR>", { desc = "Step into" })
 map("n", "<leader>do", "<cmd> DapStepOut <CR>", { desc = "Step out" })
 map("n", "<leader>dp", "<cmd> DapPause <CR>", { desc = "Pause the debugger" })
-map("n", "<leader>dq", "<cmd> DapTerminate <CR>", { desc = "Terminate the debugger" })
+-- Duplicated debug shortcuts
+-- NOTE: Depends on the terminal being used
+map("n", "<F9>", "<cmd> DapToggleBreakpoint <CR>", { desc = "Add/remove breakpoint at line" })
+map("n", "<F5>", "<cmd> DapContinue <CR>", { desc = "Start/continue the debugger" })
+map("n", "<F10>", "<cmd> DapStepOver <CR>", { desc = "Step over" })
+map("n", "<F11>", "<cmd> DapStepInto <CR>", { desc = "Step into" })
+map("n", "<F10>", "<cmd> DapStepOut <CR>", { desc = "Step out" })
+map("n", "<F6>", "<cmd> DapPause <CR>", { desc = "Pause the debugger" })
+map("n", "<S-F5>", "<cmd> DapTerminate <CR>", { desc = "Terminate the debugger" })
+map("n", "<S-F7>", "<cmd> DapEval <CR>", { desc = "Evaluate expression" })
 map(
   "n",
   "<leader>dc",
@@ -37,7 +47,7 @@ map("n", "<leader>rt", function()
     id = "run",
     pos = "sp",
     cmd = function()
-      vim.cmd("write")
+      vim.cmd "write"
       local file = vim.fn.expand "%"
       local ft_cmds = {
         cpp = "clang++ " .. file .. " -g -O0 -Wall -Wextra -Werror && ./a.out && exit",
